a62271767dd0672dbf5a0d2aaba54e1d1438ea7f
Karolina Borisova <kkaarrii1010@gmail.com>
2023-03-14 16:00:16 +0200

    Added registration

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,37 +1,65 @@
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import * as authService from '../../services/authService'
+
 import './Register.css';
 export default function Register() {
-    return (
-        <div className="form">
-            <div className="form-toggle" />
-            <div className="form-panel one">
-                <div className="form-header">
-                    <h1>Register account</h1>
-                </div> 
-                <div className="form-content">
-                <form>
-        <div class="form-group">
-          <label for="username">Username</label>
-          <input type="text" id="username" name="username" required="required"/>
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <input type="password" id="password" name="password" required="required"/>
-        </div>
-        <div className="form-group">
-          <label for="cpassword">Confirm Password</label>
-          <input type="password" id="cpassword" name="cpassword" required="required"/>
-        </div>
-        <div className="form-group">
-          <label for="email">Email Address</label>
-          <input type="email" id="email" name="email" required="required"/>
-        </div>
-        <div className="form-group">
-          <button type="submit">Register</button>
-        </div>
-      </form>
-                </div>
-            </div>
-        </div>
 
-    );
+  const { userLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const {
+      email,
+      password,
+      cpassword,
+      username,
+    } = Object.fromEntries(new FormData(e.target));
+
+    if(password !== cpassword){
+     return navigate('/error')
+    }
+
+    authService.register(email,password,cpassword,username)
+    .then(authData => userLogin(authData));
+    ;
+    navigate('/')
+    
+  }
+  return (
+    <div className="form">
+      <div className="form-toggle" />
+      <div className="form-panel one">
+        <div className="form-header">
+          <h1>Register account</h1>
+        </div>
+        <div className="form-content">
+          <form onSubmit={onSubmit}>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" id="username" name="username" required="required" />
+            </div>
+            <div className="form-group">
+              <label for="password">Password</label>
+              <input type="password" id="password" name="password" required="required" />
+            </div>
+            <div className="form-group">
+              <label for="cpassword">Confirm Password</label>
+              <input type="password" id="cpassword" name="cpassword" required="required" />
+            </div>
+            <div className="form-group">
+              <label for="email">Email Address</label>
+              <input type="email" id="email" name="email" required="required" />
+            </div>
+            <div className="form-group">
+              <button type="submit">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -5,18 +5,18 @@ export const login = (email, password) =>
     request('POST', `${baseUrl}/login`, { email, password });
 
 export const logout = async (accessToken) =>{ 
-
 try {
     const response = await fetch(`${baseUrl}/logout`, {
         headers: {
             'X-Authorization': accessToken
         }
     });
-
     return response;
 }
     catch (error) {
         console.log(error);
     }
-
 }
+
+export const register = (email, password , username) =>
+    request('POST', `${baseUrl}/register`, { email, password, username });
