e35d9990fa19023dc5bf1a03ab4b88e0a9b1d1ed
Karolina Borisova <kkaarrii1010@gmail.com>
2023-03-29 10:42:28 +0200

    .

--- src/components/EditDrawing/EditDrawing.js
+++ src/components/EditDrawing/EditDrawing.js
@@ -15,14 +15,14 @@ export default function EditDrawing() {
 
     const [imageSelected, setImageSelected] = useState("");
     const [currentDrawing, setCurrentDrawing] = useState({});
-    const [formValues, setFormValues] = useState({
-        name: '',
-        age: '',
-        category: '',
-        title: '',
-        imgUrl: ''
+    // const [formValues, setFormValues] = useState({
+    //     name: '',
+    //     age: '',
+    //     category: '',
+    //     title: '',
+    //     imgUrl: ''
 
-    })
+    // })
     const [formErrors, setFormErrors] = useState({
         name: '',
         age: '',
@@ -32,44 +32,42 @@ export default function EditDrawing() {
     })
 
 
-    const {} = useContext(DrawingContext);
-    const {drawingId} = useParams();
+    const { } = useContext(DrawingContext);
+    const { drawingId } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
         drawingService.getOneById(drawingId)
-        .then( drawingData => {
-            setCurrentDrawing(drawingData);
-        })
-        .catch(err => {
-            navigate('/error')
-        });
-
-    },[drawingId])
+            .then(drawingData => {
+                setCurrentDrawing(drawingData);
+                
+            })
+            .catch(err => {
+                navigate('/error')
+            });
+           
+            
+    }, [drawingId])
 
 
     useEffect(() => {
-        if(formValues.imgUrl != ''){
-            drawingService.edit(drawingId, formValues)
-    .then(result => {
-        console.log(result);
-        navigate(`/drawings/${result._id}`)
-    })
-    .catch(() => {
-        navigate('/error')
-    });
-        }
-        
+        drawingService.edit(drawingId, currentDrawing)
+            .then(result => {
+                console.log(result);
+                navigate(`/drawings/${result._id}`)
+            })
+            .catch(() => {
+                navigate('/error')
+            });
+    }, [currentDrawing.imgUrl])
 
-    },[formValues.imgUrl])
+    console.log("currdrawing");
+    console.log(currentDrawing);
+    console.log("Files");
+    console.log(imageSelected);
 
+    const onSubmit = async (e) => {
 
-    
-  
- 
-
-    const onSubmit =  async(e) => {
-        
         e.preventDefault();
         const imgUrl = 'https://res.cloudinary.com/dbk16pp6v/image/upload/'
         const formData = new FormData();
@@ -80,20 +78,21 @@ export default function EditDrawing() {
         console.log(cloudinaryImg)
         console.log('cloudinaryImg');
         console.log(cloudinaryImg.public_id);
-        
-        setFormValues(state => ({ ...state, imgUrl: `${imgUrl}${cloudinaryImg.public_id}` }));
-        console.log('formVAlues');
-        console.log(formValues);    
-      
-    }
 
- 
+        setCurrentDrawing(state => ({ ...state, imgUrl: `${imgUrl}${cloudinaryImg.public_id}` }));
+
+    }
+    console.log('formVAlues');
+    console.log(currentDrawing);
+
     const onChange = (e) => {
         if (e.target.name != 'imgUrl') {
-            setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
+            // setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
             setCurrentDrawing(state => ({ ...state, [e.target.name]: e.target.value }));
+
+           
         }
-        else{
+        else {
             setImageSelected(e.target.files[0]);
         }
 
@@ -101,83 +100,100 @@ export default function EditDrawing() {
     const formChangeHandler = (e) => {
         const value = e.target.value;
         const inputName = e.target.name;
-        
+
         let errors = drawingValidator(inputName, value)
 
         setFormErrors(errors);
 
     };
 
-    return         <div className="form">
-    <div className="form-toggle" />
-    <div className="form-panel one">
-        <div className="form-header">
-            <h1>Edit drawing</h1>
-        </div>
-        <div className="form-content">
-            <form onSubmit = { onSubmit }>
-                <div className="form-group">
-                    <label htmlFor="name">Artist name</label>
-                    <input
-                        type="text"
-                        required="required"
-                        name="name"
-                        id="name"
-                        placeholder="First and Last name"
-                        value={currentDrawing.name}
-                        onChange={onChange}
-                        onBlur={formChangeHandler}
-                    />
-                </div>
-                {formErrors.age &&
-                            <p className="errorMessage">{formErrors.age}</p>}
-                <div className="form-group">
-                    <label htmlFor="title">Drawing title</label>
-                    <input
-                        type="text"
-                        required="required"
-                        name="title"
-                        placeholder="Title"
-                        id="title"
-                        value={currentDrawing.title}
-                        onChange={onChange}
-                        onBlur={formChangeHandler}
-                    />
-                </div>
-                {formErrors.title &&
-                            <p className="errorMessage">{formErrors.title}</p>}
-                <div className="form-group">
-                            <label htmlFor="category">Category</label>
-                            <select  
-                            name ="category" 
-                            required="required" 
-                            id = "category"
+    return <div className="form">
+        <div className="form-toggle" />
+        <div className="form-panel one">
+            <div className="form-header">
+                <h1>Edit drawing</h1>
+            </div>
+            <div className="form-content">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Artist name</label>
+                        <input
+                            type="text"
+                            required="required"
+                            name="name"
+                            id="name"
+                            placeholder="First and Last name"
+                            value={currentDrawing.name}
+                            onChange={onChange}
+                            onBlur={formChangeHandler}
+                        />
+                    </div>
+                    {formErrors.name &&
+                        <p className="errorMessage">{formErrors.name}</p>}
+                    <div className="form-group">
+                        <label htmlFor="age">Age</label>
+                        <input
+                            type="number"
+                            required="required"
+                            name="age"
+                            id="age"
+                            min="3"
+                            max="7"
+                            value={currentDrawing.age}
+                            onChange={onChange}
+                            onBlur={formChangeHandler}
+
+                        />
+                    </div>
+                    {formErrors.age &&
+                        <p className="errorMessage">{formErrors.age}</p>}
+                    <div className="form-group">
+                        <label htmlFor="title">Drawing title</label>
+                        <input
+                            type="text"
+                            required="required"
+                            name="title"
+                            placeholder="Title"
+                            id="title"
+                            value={currentDrawing.title}
+                            onChange={onChange}
+                            onBlur={formChangeHandler}
+                        />
+                    </div>
+                    {formErrors.title &&
+                        <p className="errorMessage">{formErrors.title}</p>}
+                    <div className="form-group">
+                        <label htmlFor="category">Category</label>
+                        <select
+                            name="category"
+                            required="required"
+                            id="category"
                             value={currentDrawing.category}
                             onChange={onChange}
-                             >
-                                <option value="I group">I group (3-4 years)</option>
-                                <option value="II group">II group (4-5 years)</option>
-                                <option value="III group">III group (5-6 years)</option>
-                                <option value="IV group">IV group (6-7 years)</option>
-                            </select>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="imgUrl">Add file</label>
-                            <input
-                                type="file"
-                                name="imgUrl"
-                                id="imgUrl"
-                                
-                                onChange={onChange}
-                                
-                            />
-                        </div>
-                <div className="form-group">
-                    <button type="submit">Edit</button>
-                </div>
-            </form>
+                        >
+                            <option value="I group">I group (3-4 years)</option>
+                            <option value="II group">II group (4-5 years)</option>
+                            <option value="III group">III group (5-6 years)</option>
+                            <option value="IV group">IV group (6-7 years)</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="imgUrl">Add file</label>
+                        <input
+                            type="file"
+                            name="imgUrl"
+                            id="imgUrl"
+
+                            onChange={onChange}
+
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit">Edit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 }
